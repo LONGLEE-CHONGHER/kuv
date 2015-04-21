16f9802c0bc1060fd33c2e649185d3c30b7f07a2
HACKER_LINK <longlee.ch.dev@gmail.com>
2015-04-21 18:58:51 +0700

    kuv li

--- app/src/main/java/longlee/ch/Restaurant/OrderActivity.java
+++ app/src/main/java/longlee/ch/Restaurant/OrderActivity.java
@@ -69,7 +69,7 @@ public class OrderActivity extends ActionBarActivity {
         strNumberTable = edtTable.getText().toString().trim();
         if (strNumberTable.equals("")) {
             MyAlertDialog objMyAlertDialog = new MyAlertDialog();
-            objMyAlertDialog.alertDialog(OrderActivity.this, "TABLE NUMBER", "Please insert your table number");
+            objMyAlertDialog.alertDialog(OrderActivity.this, "YOUR ADDRESS", "Please insert your address");
         }
     }
 
@@ -82,7 +82,7 @@ public class OrderActivity extends ActionBarActivity {
                 strNumberTable = edtTable.getText().toString().trim();
                 if (strNumberTable.equals("")) {
                     MyAlertDialog objMyAlertDialog = new MyAlertDialog();
-                    objMyAlertDialog.alertDialog(OrderActivity.this, "TABLE NUMBER", "Please insert your table number");
+                    objMyAlertDialog.alertDialog(OrderActivity.this, "YOUR ADDRESS", "Please insert your address");
                 } else {
                     alertAmountOrder();
                     strFood = strMenu[position];
